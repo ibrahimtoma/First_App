1926fcfb38f1662e3750c7f64c93b5aafac40318
ElGam'eya App <ibrahimtoma04@gmail.com>
2017-08-12 11:07:52 +0200

    Added Node

--- src/App.js
+++ src/App.js
@@ -2,14 +2,10 @@ import React, { Component } from 'react';
 import logo from './feed_logo.png';
 import './App.css';
 import Header from './Header';
-import { Link } from 'react-router';
-import { LoginLink } from 'react-stormpath';
-import DocumentTitle from 'react-document-title';
-import MasterPage from './index';
-export LoginPage from './SlideMenu';
-import bar from './bar';
 
-bar();
+
+
+
 
 
 export default class Main extends Component {
@@ -20,16 +16,7 @@ export default class Main extends Component {
 
                 <div className="App-header">
                     <img src={logo} className="App-logo" alt="logo"/>
-                    <DocumentTitle title='My React App'>
-                        <div className='MasterPage'>
-                            <Header />
-                            { this.props.children }
-                            <Route path='/' component={MasterPage}>
-                                <LoginRoute path='/login' component={LoginPage} />
-                                <Route path='/hello' component={HelloPage} />
-                            </Route>
-                        </div>
-                    </DocumentTitle>
+
                 </div>
                 <p className="App-intro">
                     <h1>Hello From Feed</h1>
--- src/index.js
+++ src/index.js
@@ -2,20 +2,10 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-import { Link } from 'react-router';
-import { LoginLink } from 'react-stormpath';
-import DocumentTitle from 'react-document-title';
-export MasterPage from './index';
-export LoginPage from './SlideMenu';
+
 
 ReactDOM.render(
-    <Route path='/' component={MasterPage}>
-        <Route path='/' component={MasterPage}>
-            <IndexRoute component={IndexPage} />
-        </Route>
-        <Route path='/hello' component={HelloPage} />
-    </Route>,
-    <LoginRoute path='/login' component={LoginPage} />,
+
     <App />,
   document.getElementById('root'),
 );
